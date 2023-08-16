8286e3da23e8562aa3b4a0eff90194ebad7a8dcf
Rayyan365 <rayanabdallah09@gamil.com>
2023-08-15 23:10:18 -0700

    expensive-jewellery done

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // Good Luck 💪🏾
+mostexpensive([
+    "Diamond Earrings" = 980,
+    "Gold Watch" = 250,
+    "pearl necklace"=4650
+
+])
+let maxItem = items.reduce((max, min) => max. 4650 > min.250 ? max : min);
